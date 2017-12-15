407657ae9791828764556151adc0f88472ec63cb
za-chujun <1571970439@qq.com>
2017-12-15 20:19:22 +0800

    jdk change to 1.7 version

---
+++ src/main/java/com/jun/chu/java/collection/MyHashMap.java
@@ -0,0 +1,10 @@
+package com.jun.chu.java.collection;
+
+/**
+ * Created by chujun on 2017/12/15.
+ */
+public class MyHashMap {
+    public static void main(String[] args){
+        
+    }
+}
--- src/test/java/com/jun/chu/java/cache/guava/LoadingCacheTest.java
+++ src/test/java/com/jun/chu/java/cache/guava/LoadingCacheTest.java
@@ -1,15 +1,8 @@
 package com.jun.chu.java.cache.guava;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.collect.Lists;
-import org.junit.Test;
-
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicInteger;
+
+//import java.util.Optional;
 
 /**
  * Created by chujun on 2017/9/22.
@@ -17,76 +10,76 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class LoadingCacheTest {
     private static AtomicInteger                     count              = new AtomicInteger();
 
-    LoadingCache<String, BigExpense>                 cache              = CacheBuilder.newBuilder()
-            .build(new CacheLoader<String, BigExpense>() {
-                                                                                    public BigExpense load(String key) {
-                                                                                        return createBigExpense(key);
-                                                                                    }
-                                                                                });
-    //解决LoadingCache不允许返回null值的方法
-    LoadingCache<String, Optional<BigExpense>>       cacheNullValue     = CacheBuilder.newBuilder()
-            .build(new CacheLoader<String, Optional<BigExpense>>() {
-                                                                                    public Optional<BigExpense> load(String key) {
-                                                                                        //return createBigExpense(key);
-                                                                                        return Optional.ofNullable(
-                                                                                                createBigExpense(key));
-                                                                                    }
-                                                                                });
-
-    LoadingCache<String, Optional<List<BigExpense>>> cacheListNullValue = CacheBuilder.newBuilder()
-            .build(new CacheLoader<String, Optional<List<BigExpense>>>() {
-
-                                                                                    public Optional<List<BigExpense>> load(String key) {
-                                                                                        //return createBigExpense(key);
-                                                                                        return Optional.ofNullable(
-                                                                                                Lists.newArrayList(
-                                                                                                        createBigExpense(
-                                                                                                                key),
-                                                                                                        createBigExpense(
-                                                                                                                key)));
-                                                                                    }
-
-                                                                                });
-
-    @Test
-    public void case01_get() {
-        try {
-            System.out.println(cache.get("chujun"));
-            Optional<BigExpense> optional = cacheNullValue.get("cj");
-            //两者等效
-            System.out.println(optional.isPresent() ? optional.get() : null);
-            System.out.println(optional.orElse(null));
-
-            System.out.println(cacheListNullValue.get("cjq"));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void case01_get_value_is_null() throws ExecutionException {
-        try {
-            //默认方式不允许value为null
-            System.out.println(cache.get(""));
-            //com.google.common.cache.CacheLoader$InvalidCacheLoadException: CacheLoader returned null for key .
-        } catch (CacheLoader.InvalidCacheLoadException e) {
-            e.printStackTrace();
-            if (!e.getMessage().contains("CacheLoader returned null for key")) {
-                throw e;
-            }
-        }
-    }
-
-    @Test
-    public void case02_get_value_is_Absent() {
-        try {
-            Optional<BigExpense> optional = cacheNullValue.get("");
-            System.out.println(optional.isPresent() ? optional.get() : null);
-            System.out.println(optional.orElse(null));
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-    }
+//    LoadingCache<String, BigExpense>                 cache              = CacheBuilder.newBuilder()
+//            .build(new CacheLoader<String, BigExpense>() {
+//                                                                                    public BigExpense load(String key) {
+//                                                                                        return createBigExpense(key);
+//                                                                                    }
+//                                                                                });
+//    //解决LoadingCache不允许返回null值的方法
+//    LoadingCache<String, Optional<BigExpense>>       cacheNullValue     = CacheBuilder.newBuilder()
+//            .build(new CacheLoader<String, Optional<BigExpense>>() {
+//                                                                                    public Optional<BigExpense> load(String key) {
+//                                                                                        //return createBigExpense(key);
+//                                                                                        return Optional.ofNullable(
+//                                                                                                createBigExpense(key));
+//                                                                                    }
+//                                                                                });
+//
+//    LoadingCache<String, Optional<List<BigExpense>>> cacheListNullValue = CacheBuilder.newBuilder()
+//            .build(new CacheLoader<String, Optional<List<BigExpense>>>() {
+//
+//                                                                                    public Optional<List<BigExpense>> load(String key) {
+//                                                                                        //return createBigExpense(key);
+//                                                                                        return Optional.ofNullable(
+//                                                                                                Lists.newArrayList(
+//                                                                                                        createBigExpense(
+//                                                                                                                key),
+//                                                                                                        createBigExpense(
+//                                                                                                                key)));
+//                                                                                    }
+//
+//                                                                                });
+//
+//    @Test
+//    public void case01_get() {
+//        try {
+//            System.out.println(cache.get("chujun"));
+//            Optional<BigExpense> optional = cacheNullValue.get("cj");
+//            //两者等效
+//            System.out.println(optional.isPresent() ? optional.get() : null);
+//            System.out.println(optional.orElse(null));
+//
+//            System.out.println(cacheListNullValue.get("cjq"));
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    @Test
+//    public void case01_get_value_is_null() throws ExecutionException {
+//        try {
+//            //默认方式不允许value为null
+//            System.out.println(cache.get(""));
+//            //com.google.common.cache.CacheLoader$InvalidCacheLoadException: CacheLoader returned null for key .
+//        } catch (CacheLoader.InvalidCacheLoadException e) {
+//            e.printStackTrace();
+//            if (!e.getMessage().contains("CacheLoader returned null for key")) {
+//                throw e;
+//            }
+//        }
+//    }
+//
+//    @Test
+//    public void case02_get_value_is_Absent() {
+//        try {
+//            Optional<BigExpense> optional = cacheNullValue.get("");
+//            System.out.println(optional.isPresent() ? optional.get() : null);
+//            System.out.println(optional.orElse(null));
+//        } catch (ExecutionException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     private BigExpense createBigExpense(String key) {
         if (null == key || "".equals(key)) {

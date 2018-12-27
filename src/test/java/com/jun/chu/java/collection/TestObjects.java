a3287c883d961437d537a34ffda52d6a56eaa55c
za-chujun <1571970439@qq.com>
2018-12-27 14:47:16 +0800

    TestObjects

---
+++ src/test/java/com/jun/chu/java/collection/TestObjects.java
@@ -0,0 +1,17 @@
+package com.jun.chu.java.collection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Objects;
+
+/**
+ * @author jun.chu
+ * @date 2018-12-26 18:42
+ */
+public class TestObjects {
+    @Test
+    public void testEquals(){
+        Assert.assertEquals(false, Objects.equals(true,null));
+    }
+}

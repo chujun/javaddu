5abe69f038ec1b33316e24875543b4bc4d6cfa0c
za-chujun <1571970439@qq.com>
2019-01-14 14:28:02 +0800

    StreamTest concat

---
+++ src/test/java/com/jun/chu/java/lambda/StreamTest.java
@@ -0,0 +1,25 @@
+package com.jun.chu.java.lambda;
+
+import com.google.common.collect.Lists;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author jun.chu
+ * @date 2019-01-14 14:22
+ */
+public class StreamTest {
+    @Test
+    public void testConcat() {
+        List<Integer> first = Collections.singletonList(1);
+        List<Integer> two = Lists.newArrayList(2, 3, 4, 5);
+        List<Integer> result = Stream.concat(first.stream(), two.stream()).collect(Collectors.toList());
+        System.out.println(result);
+        Assert.assertEquals("[1, 2, 3, 4, 5]",result.toString());
+    }
+}

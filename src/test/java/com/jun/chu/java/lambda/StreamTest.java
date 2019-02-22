277b7372a7db0fa7a4ab2790e3f143bd08600776
za-chujun <1571970439@qq.com>
2019-02-22 09:52:59 +0800

    test sort

--- src/test/java/com/jun/chu/java/lambda/StreamTest.java
+++ src/test/java/com/jun/chu/java/lambda/StreamTest.java
@@ -5,6 +5,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -21,7 +22,7 @@ public class StreamTest {
         List<Integer> result = Stream.concat(first.stream(), two.stream()).collect(Collectors.toList());
         System.out.println(result);
         Assert.assertEquals("[1, 2, 3, 4, 5]", result.toString());
-        first = Lists.newArrayList(1,11);
+        first = Lists.newArrayList(1, 11);
         result = Stream.concat(first.stream(), two.stream()).collect(Collectors.toList());
         System.out.println(result);
         Assert.assertEquals("[1, 11, 2, 3, 4, 5]", result.toString());
@@ -31,5 +32,18 @@ public class StreamTest {
     public void testEmptyListCollect() {
         List<Integer> list = Lists.newArrayList();
         Assert.assertEquals(0, list.stream().collect(Collectors.toList()).size());
+    }
+
+    @Test
+    public void testSorted() {
+        List<Integer> list = Lists.newArrayList(1, 3, 4, 2);
+        //这个排序影响原来的集合顺序
+        list.sort(Comparator.comparing(item -> item));
+        System.out.println(list);
+        Assert.assertEquals(Lists.newArrayList(1, 2, 3, 4), list);
+        list = Lists.newArrayList(1, 3, 4, 2);
+        //这个stream流排序不影响原来的集合顺序
+        list.stream().sorted(Comparator.comparing(item -> item));
+        Assert.assertEquals(Lists.newArrayList(1, 3, 4, 2), list);
     }
 }

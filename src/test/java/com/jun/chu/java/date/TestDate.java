c7cccb27eb02a680d4bbd09bd93dc342a8ca4824
za-chujun <1571970439@qq.com>
2019-03-19 19:30:40 +0800

    add TestDate

--- src/test/java/com/jun/chu/java/date/TestDate.java
+++ src/test/java/com/jun/chu/java/date/TestDate.java
@@ -19,6 +19,23 @@ public class TestDate {
         System.out.println(setDateForLastSecond235959(date));
     }
 
+    @Test
+    public void test(){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.DATE, -30);
+        Date thirtyDaysAgo = calendar.getTime();
+        System.out.println(thirtyDaysAgo);
+
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.setTime(new Date());
+        calendar2.add(Calendar.DAY_OF_MONTH, -30);
+        Date thirtyDaysAgo2 = calendar2.getTime();
+        System.out.println(thirtyDaysAgo2);
+
+        System.out.println(String.format("%sR", "2019031909455188415306"));
+    }
+
     private Date setDateForLastSecond235959(Date date) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);

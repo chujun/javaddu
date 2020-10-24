afc41d7b9b3103bf72516ce9461d8ba327ca37c3
jun.chu <1571970439@qq.com>
2020-10-24 17:18:07 +0800

    refactor(userservicetest): 采用BDD测试方式Behavior-Driven Development

--- src/test/java/com/jun/chu/java/mockito/UserServiceTest.java
+++ src/test/java/com/jun/chu/java/mockito/UserServiceTest.java
@@ -4,6 +4,7 @@ import com.jun.chu.java.mockito.domain.User;
 import com.jun.chu.java.mockito.service.UserService;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
 /**
@@ -27,5 +28,32 @@ public class UserServiceTest {
         Mockito.verify(userService).findByName("b");
         Mockito.verify(userService, Mockito.times(0))
             .findByName("c");
+        //4.2调用次数验证
+        Assert.assertNull(userService.findByName("b"));
+        Mockito.verify(userService, Mockito.times(2))
+            .findByName("b");
+    }
+
+    @Test
+    public void testBDD() {
+        //采用BDD测试方式Behavior-Driven Development
+        //1.mock具体接口/类
+        UserService userService = BDDMockito.mock(UserService.class);
+        //2.在执行之前stub
+        User stubUser = new User("1", "12");
+        BDDMockito.given(userService.findByName("1")).willReturn(stubUser);
+        //3.执行
+        Assert.assertEquals(stubUser.toString(), userService.findByName("1").toString());
+        Assert.assertNull(userService.findByName("b"));
+        //4.验证
+        BDDMockito.then(userService).should().findByName("1");
+        BDDMockito.then(userService).should().findByName("b");
+        BDDMockito.then(userService).should(BDDMockito.times(0))
+            .findByName("c");
+        //4.2调用次数验证
+        Assert.assertNull(userService.findByName("b"));
+        BDDMockito.then(userService).should(BDDMockito.times(2))
+            .findByName("b");
+
     }
 }

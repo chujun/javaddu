a4de628e975ef12d859d8afec0319379b9c3d484
jun.chu <1571970439@qq.com>
2021-07-08 16:07:51 +0800

    refactor(liststest): modify

--- src/test/java/com/jun/chu/java/collection/list/ListsTest.java
+++ src/test/java/com/jun/chu/java/collection/list/ListsTest.java
@@ -2,6 +2,7 @@ package com.jun.chu.java.collection.list;
 
 import com.google.common.collect.Lists;
 import org.junit.Test;
+import org.mapstruct.ap.shaded.freemarker.template.utility.CollectionUtils;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,5 +23,17 @@ public class ListsTest {
             return item.size();
         }).collect(Collectors.toList());
         System.out.println(result);
+    }
+
+    @Test
+    public void testRetain(){
+        List<Integer> list1=Lists.newArrayList(1,2,3);
+        List<Integer> list2 =Lists.newArrayList(2,3,4);
+        List<Integer> list3=Lists.newArrayList(5);
+        List<Integer> list4=Lists.newArrayList();
+        System.out.println(list1.retainAll(list2));
+        System.out.println(list1.retainAll(list3));
+        System.out.println(list1.retainAll(list4));
+
     }
 }

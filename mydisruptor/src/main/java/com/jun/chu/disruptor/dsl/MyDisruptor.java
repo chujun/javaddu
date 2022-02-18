24dd5c827bf1ea6465acd6820079c24a096abcfe
jun.chu <1571970439@qq.com>
2022-02-18 18:03:37 +0800

    refactor(mydisruptor): create

--- mydisruptor/src/main/java/com/jun/chu/disruptor/dsl/MyDisruptor.java
+++ mydisruptor/src/main/java/com/jun/chu/disruptor/dsl/MyDisruptor.java
@@ -1,9 +1,62 @@
 package com.jun.chu.disruptor.dsl;
 
+import com.jun.chu.disruptor.MyEventFactory;
+import com.jun.chu.disruptor.MyExceptionHandler;
+import com.jun.chu.disruptor.MyRingBuffer;
+import com.jun.chu.disruptor.MyWaitStrategy;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * @author chujun
  * @date 2022/2/16
  */
 public class MyDisruptor<T> {
+    private final MyRingBuffer<T> ringBuffer;
+    private final Executor executor;
+    private final MyConsumerRepository<T> consumerRepository = new MyConsumerRepository<>();
+    private final AtomicBoolean started = new AtomicBoolean(false);
+    private MyExceptionHandler<? super T> exceptionHandler = new MyExceptionHandlerWrapper<>();
 
+    @Deprecated
+    public MyDisruptor(final MyEventFactory<T> eventFactory, final int ringBufferSize, final Executor executor)
+    {
+        this(MyRingBuffer.createMultiProducer(eventFactory, ringBufferSize), executor);
+    }
+
+    @Deprecated
+    public MyDisruptor(
+        final MyEventFactory<T> eventFactory,
+        final int ringBufferSize,
+        final Executor executor,
+        final MyProducerType producerType,
+        final MyWaitStrategy waitStrategy)
+    {
+        this(MyRingBuffer.create(producerType, eventFactory, ringBufferSize, waitStrategy), executor);
+    }
+
+    public MyDisruptor(final MyEventFactory<T> eventFactory, final int ringBufferSize, final ThreadFactory threadFactory)
+    {
+        this(MyRingBuffer.createMultiProducer(eventFactory, ringBufferSize), new MyBasicExecutor(threadFactory));
+    }
+
+    public MyDisruptor(
+        final MyEventFactory<T> eventFactory,
+        final int ringBufferSize,
+        final ThreadFactory threadFactory,
+        final MyProducerType producerType,
+        final MyWaitStrategy waitStrategy)
+    {
+        this(
+            MyRingBuffer.create(producerType, eventFactory, ringBufferSize, waitStrategy),
+            new MyBasicExecutor(threadFactory));
+    }
+
+    private MyDisruptor(final MyRingBuffer<T> ringBuffer, final Executor executor)
+    {
+        this.ringBuffer = ringBuffer;
+        this.executor = executor;
+    }
 }

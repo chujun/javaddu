0eb1c9ee071a2c6c8559db2610d7d59c30eed2ea
jun.chu <1571970439@qq.com>
2022-03-31 19:50:29 +0800

    refactor(collectionstomaptest): testToMapSum

--- src/test/java/com/jun/chu/java/lambda/CollectionsToMapTest.java
+++ src/test/java/com/jun/chu/java/lambda/CollectionsToMapTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class CollectionsToMapTest {
@@ -18,7 +19,7 @@ public class CollectionsToMapTest {
         List<PropertyOptionValue> propertyOptionValues = initData();
         try {
             Map<Integer, PropertyOptionValue> collect = propertyOptionValues.stream().filter(item -> null != item)
-                    .collect(Collectors.toMap(PropertyOptionValue::getId, item -> item));
+                .collect(Collectors.toMap(PropertyOptionValue::getId, item -> item));
         } catch (IllegalStateException e) {
             //throwingMerger()
             Assert.assertEquals(true, e.getMessage().contains("Duplicate key"));
@@ -30,18 +31,26 @@ public class CollectionsToMapTest {
         //允许key重复
         List<PropertyOptionValue> propertyOptionValues = initData();
         Map<Integer, PropertyOptionValue> map = propertyOptionValues.stream().filter(item -> null != item)
-                .collect(Collectors.toMap(PropertyOptionValue::getId, item -> item, (a, b) -> b));
+            .collect(Collectors.toMap(PropertyOptionValue::getId, item -> item, (a, b) -> b));
         PropertyOptionValue propertyOptionValue = map.get(1);
         Assert.assertEquals(1, propertyOptionValue.getId().intValue());
         Assert.assertEquals("8G", propertyOptionValue.getName());
         Assert.assertEquals(2, propertyOptionValue.getVersion().intValue());
 
-        map = propertyOptionValues.stream().filter(item -> null != item)
-                .collect(Collectors.toMap(PropertyOptionValue::getId, item -> item, (a, b) -> a));
+        map = propertyOptionValues.stream().filter(Objects::nonNull)
+            .collect(Collectors.toMap(PropertyOptionValue::getId, item -> item, (a, b) -> a));
         propertyOptionValue = map.get(1);
         Assert.assertEquals(1, propertyOptionValue.getId().intValue());
         Assert.assertEquals("8G", propertyOptionValue.getName());
         Assert.assertEquals(1, propertyOptionValue.getVersion().intValue());
+    }
+
+    @Test
+    public void testToMapSum() {
+        Map<Integer, Integer> idSumMap = initData().stream()
+            .collect(Collectors.toMap(PropertyOptionValue::getId,
+                PropertyOptionValue::getVersion, Integer::sum));
+        Assert.assertEquals(idSumMap.toString(), "{1=3, 2=6, 3=11}");
     }
 
     public List<PropertyOptionValue> initData() {
@@ -50,6 +59,8 @@ public class CollectionsToMapTest {
         result.add(new PropertyOptionValue(2, "16G", new Date(), 1));
         result.add(new PropertyOptionValue(3, "32G", new Date(), 1));
         result.add(new PropertyOptionValue(1, "8G", new Date(), 2));
+        result.add(new PropertyOptionValue(2, "32G", new Date(), 5));
+        result.add(new PropertyOptionValue(3, "64G", new Date(), 10));
         return result;
     }
 
@@ -58,9 +69,9 @@ public class CollectionsToMapTest {
     static class PropertyOptionValue {
         private Integer id;
 
-        private String  name;
+        private String name;
 
-        private Date    createAt;
+        private Date createAt;
 
         private Integer version;
     }

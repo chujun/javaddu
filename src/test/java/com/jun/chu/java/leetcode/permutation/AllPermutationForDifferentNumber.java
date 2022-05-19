25b4a0cdb77d471a4b27ccf1cf27717d1b3b655c
jun.chu <1571970439@qq.com>
2022-05-19 13:39:09 +0800

    refactor(allpermutationfordifferentnumber): tree

--- src/test/java/com/jun/chu/java/leetcode/permutation/AllPermutationForDifferentNumber.java
+++ src/test/java/com/jun/chu/java/leetcode/permutation/AllPermutationForDifferentNumber.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 /**
  * 针对不重复数字组合的全排列
@@ -30,6 +31,23 @@ public class AllPermutationForDifferentNumber {
         Assert.assertEquals(permute(new int[]{1, 2, 3, 4}).toString(), "[[1, 2, 3, 4], [1, 2, 4, 3], [1, 3, 2, 4], [1, 3, 4, 2], [1, 4, 2, 3], [1, 4, 3, 2], [2, 1, 3, 4], [2, 1, 4, 3], [2, 3, 1, 4], [2, 3, 4, 1], [2, 4, 1, 3], [2, 4, 3, 1], [3, 1, 2, 4], [3, 1, 4, 2], [3, 2, 1, 4], [3, 2, 4, 1], [3, 4, 1, 2], [3, 4, 2, 1], [4, 1, 2, 3], [4, 1, 3, 2], [4, 2, 1, 3], [4, 2, 3, 1], [4, 3, 1, 2], [4, 3, 2, 1]]");
     }
 
+    @Test
+    public void testPermuteV2() {
+        System.out.println(permuteV2(new int[]{1}));
+        System.out.println(permuteV2(new int[]{1, 2}));
+        System.out.println(permuteV2(new int[]{1, 2, 3}));
+        List<List<Integer>> result = permuteV2(new int[]{1, 2, 3, 4});
+        System.out.println(result);
+        System.out.println(result.size());
+        Assert.assertEquals(permuteV2(new int[]{1}).toString(), "[[1]]");
+        Assert.assertEquals(permuteV2(new int[]{1, 2}).toString(), "[[1, 2], [2, 1]]");
+        Assert.assertEquals(permuteV2(new int[]{1, 2, 3}).toString(), "[[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]");
+        Assert.assertEquals(permuteV2(new int[]{1, 2, 3, 4}).toString(), "[[1, 2, 3, 4], [1, 2, 4, 3], [1, 3, 2, 4], [1, 3, 4, 2], [1, 4, 2, 3], [1, 4, 3, 2], [2, 1, 3, 4], [2, 1, 4, 3], [2, 3, 1, 4], [2, 3, 4, 1], [2, 4, 1, 3], [2, 4, 3, 1], [3, 1, 2, 4], [3, 1, 4, 2], [3, 2, 1, 4], [3, 2, 4, 1], [3, 4, 1, 2], [3, 4, 2, 1], [4, 1, 2, 3], [4, 1, 3, 2], [4, 2, 1, 3], [4, 2, 3, 1], [4, 3, 1, 2], [4, 3, 2, 1]]");
+    }
+
+
+    /////////////////////////////方法一暴力遍历\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
     /**
      * 全排列
      *
@@ -41,29 +59,119 @@ public class AllPermutationForDifferentNumber {
         }
         List<List<Integer>> result = new ArrayList<>();
 
-        permute(result, new ArrayList<>(), OriginJDKUtil.newCopy(nums));
+        permute(result, new ArrayList<>(), OriginJDKUtil.newCopyList(nums));
         return result;
     }
 
     private void permute(List<List<Integer>> result,
                          List<Integer> toBeAddList,
                          List<Integer> remainList) {
+        if (0 == remainList.size()) {
+            return;
+        }
         //有限解
         if (1 == remainList.size()) {
             //注意不要破坏源数据结构
+            //这儿有copy成本
             List<Integer> onePermutation = OriginJDKUtil.newCopy(toBeAddList, toBeAddList.size() + 1);
             onePermutation.addAll(remainList);
             result.add(onePermutation);
+            return;
         }
         //递归解
         for (int i = 0; i < remainList.size(); i++) {
             Integer data = remainList.get(i);
+            //这儿有copy成本
             List<Integer> destToBeAddedList = OriginJDKUtil.newCopy(toBeAddList, toBeAddList.size() + 1);
             destToBeAddedList.add(data);
+            //这儿有copy成本
             List<Integer> destRemainList = OriginJDKUtil.newCopy(remainList, remainList.size());
             destRemainList.remove(i);
             permute(result, destToBeAddedList, destRemainList);
         }
+    }
+    /////////////////////////////方法一暴力遍历\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+    /////////////////////////////方法二使用树结构简化复制成本\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+    /**
+     * 使用树结构存储中间计算过程，减少复制成本
+     */
+    private List<List<Integer>> permuteV2(int[] nums) {
+        if (null == nums || 0 == nums.length) {
+            return Collections.emptyList();
+        }
+        TreeNode<Integer> currentNode = new TreeNode<>(null);
+        Tree<Integer> tree = new Tree<>(currentNode, true);
+        permuteV2(currentNode, OriginJDKUtil.newCopySet(nums));
+        return tree.toList();
+    }
+
+    private void permuteV2(TreeNode<Integer> node,
+                           Set<Integer> remainSet) {
+        if (0 == remainSet.size()) {
+            return;
+        }
+        //有限解
+        if (1 == remainSet.size()) {
+            TreeNode<Integer> leftNode = new TreeNode<>(remainSet.stream().findFirst().orElse(null));
+            node.addNode(leftNode);
+            return;
+        }
+        for (Integer data : remainSet) {
+            TreeNode<Integer> currentNode = new TreeNode<>(data);
+            node.addNode(currentNode);
+            //这儿有copy成本
+            Set<Integer> destRemainSet = OriginJDKUtil.newCopy(remainSet);
+            destRemainSet.remove(data);
+            permuteV2(currentNode, destRemainSet);
+        }
+    }
+
+    public static class Tree<T> {
+        private TreeNode<T> rootNode;
+
+        private boolean rootNodeEmpty;
+
+        public Tree(final TreeNode<T> rootNode, final boolean rootNodeEmpty) {
+            this.rootNode = rootNode;
+            this.rootNodeEmpty = rootNodeEmpty;
+        }
+
+        public List<List<T>> toList() {
+            if (null == rootNode) {
+                return Collections.emptyList();
+            }
+
+        }
+    }
+
+    public static class TreeNode<T> {
+        private T num;
+        private List<TreeNode<T>> nextNodes;
+
+        public TreeNode(final T num) {
+            this.num = num;
+        }
+
+        public TreeNode(final T num, final List<TreeNode<T>> nextNodes) {
+            this.num = num;
+            this.nextNodes = nextNodes;
+        }
+
+        public void addNode(TreeNode<T> treeNode) {
+            if (null == nextNodes) {
+                nextNodes = new ArrayList<>();
+            }
+            nextNodes.add(treeNode);
+        }
+
+        public T getNum() {
+            return num;
+        }
+
+        public List<TreeNode<T>> getNextNodes() {
+            return nextNodes;
+        }
     }
 }
--- src/test/java/com/jun/chu/java/leetcode/tools/OriginJDKUtil.java
+++ src/test/java/com/jun/chu/java/leetcode/tools/OriginJDKUtil.java
@@ -1,7 +1,9 @@
 package com.jun.chu.java.leetcode.tools;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * leetcode尽量不用第三方jar包
@@ -10,22 +12,40 @@ import java.util.List;
  * @date 2022/5/19
  */
 public class OriginJDKUtil {
-    public static List<Integer> newCopy(List<Integer> src, int initNumber) {
+    public static <T> List<T> newCopy(List<T> src, int initNumber) {
         if (null == src || 0 == src.size()) {
             return new ArrayList<>();
         }
-        List<Integer> destList = new ArrayList<>(initNumber);
-        for (Integer i : src) {
-            destList.add(i);
-        }
+        List<T> destList = new ArrayList<>(initNumber);
+        destList.addAll(src);
         return destList;
     }
 
-    public static List<Integer> newCopy(int[] src) {
+    public static <T> Set<T> newCopy(Set<T> src) {
+        if (null == src || 0 == src.size()) {
+            return new HashSet<>();
+        }
+        Set<T> destSet = new HashSet<>(src.size());
+        destSet.addAll(src);
+        return destSet;
+    }
+
+    public static List<Integer> newCopyList(int[] src) {
         if (null == src || 0 == src.length) {
             return new ArrayList<>();
         }
         List<Integer> dest = new ArrayList<>(src.length);
+        for (final int num : src) {
+            dest.add(num);
+        }
+        return dest;
+    }
+
+    public static Set<Integer> newCopySet(int[] src) {
+        if (null == src || 0 == src.length) {
+            return new HashSet<>();
+        }
+        Set<Integer> dest = new HashSet<>(src.length);
         for (final int num : src) {
             dest.add(num);
         }

e9febddea09f3040c7ccfe04331d279c5d8a5d43
jun.chu <1571970439@qq.com>
2022-05-20 20:29:20 +0800

    refactor(binarysearch): search

---
+++ src/test/java/com/jun/chu/java/leetcode/search/BinarySearch.java
@@ -0,0 +1,47 @@
+package com.jun.chu.java.leetcode.search;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * [704. leetcode 二分查找](https://leetcode.cn/problems/binary-search/)
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
+ * 没有重复元素
+ *
+ * @author chujun
+ * @date 2022/5/20
+ */
+public class BinarySearch {
+    private static int NOT_FIND = -1;
+
+    @Test
+    public void test() {
+        Assert.assertEquals(1, search(new int[]{-10, -5, 0, 1, 2}, -5));
+        Assert.assertEquals(-1, search(new int[]{-10, -5, 0, 1, 2}, 3));
+        Assert.assertEquals(0, search(new int[]{-5, 0, 1, 2}, -5));
+        Assert.assertEquals(-1, search(new int[]{-5, 0, 1, 2}, -4));
+        ;
+    }
+
+    /**
+     * 写法一:明确区间定义[left,right],左闭右闭
+     * 循环不变量
+     */
+    public int search(int[] nums, int target) {
+        if (null == nums || 0 == nums.length) {
+            return NOT_FIND;
+        }
+        int left = 0, right = nums.length - 1, middle = (left + right) >> 1;
+        for (; left <= right; middle = (left + right) >> 1) {
+            //左闭右闭区间，明确left,right相等情况下也可产生满足条件的结果
+            if (nums[middle] == target) {
+                return middle;
+            } else if (nums[middle] > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        return NOT_FIND;
+    }
+}

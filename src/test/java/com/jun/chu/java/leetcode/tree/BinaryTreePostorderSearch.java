2d062ea3515bb6e8e898b37f3811df60c1e952e4
jun.chu <1571970439@qq.com>
2022-05-23 17:52:48 +0800

    refactor(binarytreepostordersearch): postorderTraversalForRevert

--- src/test/java/com/jun/chu/java/leetcode/tree/BinaryTreePostorderSearch.java
+++ src/test/java/com/jun/chu/java/leetcode/tree/BinaryTreePostorderSearch.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Stack;
 
@@ -70,7 +71,7 @@ public class BinaryTreePostorderSearch {
     }
 
     private List<Integer> postorderTraversalForRevert(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
+        LinkedList<Integer> result = new LinkedList<>();
         postorderTraversalForRevert(result, root);
         return result;
     }
@@ -84,26 +85,24 @@ public class BinaryTreePostorderSearch {
      * 逆后序遍历为：1 4 2 3 5
      * **比较先序遍历(中左右)和逆后续遍历顺序可知,逆后续遍历是中右左,**
      */
-    private void postorderTraversalForRevert(List<Integer> result, TreeNode treeNode) {
+    private void postorderTraversalForRevert(LinkedList<Integer> result, TreeNode treeNode) {
         if (null == treeNode) {
             return;
         }
-        List<Integer> tmpResult = new ArrayList<>();
         Stack<TreeNode> stack = new Stack<>();
         stack.add(treeNode);
         while (!stack.empty()) {
             TreeNode pop = stack.pop();
-            tmpResult.add(pop.val);
+            //元素插入头部，这样做的好处是减少一次逆向遍历结果集消耗
+            result.addFirst(pop.val);
             if (null != pop.left) {
+                //与先序遍历相比,左节点先入栈,后出栈
                 stack.push(pop.left);
             }
             if (null != pop.right) {
+                //与先序遍历相比,右节点后入栈,先出栈
                 stack.push(pop.right);
             }
-        }
-        //这儿还是有额外的On遍历耗时
-        for (int i = tmpResult.size() - 1; i >= 0; i--) {
-            result.add(tmpResult.get(i));
         }
     }
 

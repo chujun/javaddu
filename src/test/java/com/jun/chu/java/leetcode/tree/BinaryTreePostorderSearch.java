2f62d9523f8f52200db78c3e225e82c1eed189a2
jun.chu <1571970439@qq.com>
2022-05-23 16:05:15 +0800

    refactor(binarytreepostordersearch): postorderTraversalForRevert

--- src/test/java/com/jun/chu/java/leetcode/tree/BinaryTreePostorderSearch.java
+++ src/test/java/com/jun/chu/java/leetcode/tree/BinaryTreePostorderSearch.java
@@ -1,7 +1,11 @@
 package com.jun.chu.java.leetcode.tree;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 /**
  * 二叉树的后序遍历
@@ -15,6 +19,35 @@ import java.util.List;
  * @date 2022/5/21
  */
 public class BinaryTreePostorderSearch {
+    @Test
+    public void test() {
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        treeNode1.right = treeNode2;
+        treeNode2.left = treeNode3;
+        List<Integer> result = postorderTraversalForRevert(treeNode1);
+        Assert.assertEquals("[3, 2, 1]", result.toString());
+
+    }
+
+    @Test
+    public void test2() {
+        TreeNode treeNode1 = new TreeNode(1);
+        TreeNode treeNode2 = new TreeNode(2);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(4);
+        TreeNode treeNode5 = new TreeNode(5);
+        treeNode1.left = treeNode2;
+        treeNode1.right = treeNode4;
+        treeNode2.left = treeNode5;
+        treeNode2.right = treeNode3;
+        List<Integer> result = postorderTraversalForRevert(treeNode1);
+        Assert.assertEquals("[5, 3, 2, 4, 1]", result.toString());
+
+    }
+
+
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         postorderTraversal(result, root);
@@ -29,6 +62,44 @@ public class BinaryTreePostorderSearch {
         postorderTraversal(result, treeNode.right);
         //最后访问中间节点
         result.add(treeNode.val);
+    }
+
+    private List<Integer> postorderTraversalForRevert(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        postorderTraversalForRevert(result, root);
+        return result;
+    }
+
+    /**
+     * 1-->2(左),4(右),2-->5(左),3(右)
+     * 后序遍历为：5 3 2 4 1
+     * <p>
+     * 先序遍历为：1 2 5 3 4
+     * <p>
+     * 逆后序遍历为：1 4 2 3 5
+     * **比较先序遍历(中左右)和逆后续遍历顺序可知,逆后续遍历是中右左,**
+     */
+    private void postorderTraversalForRevert(List<Integer> result, TreeNode treeNode) {
+        if (null == treeNode) {
+            return;
+        }
+        List<Integer> tmpResult = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(treeNode);
+        while (!stack.empty()) {
+            TreeNode pop = stack.pop();
+            tmpResult.add(pop.val);
+            if (null != pop.left) {
+                stack.push(pop.left);
+            }
+            if (null != pop.right) {
+                stack.push(pop.right);
+            }
+        }
+        //这儿还是有额外的On遍历耗时
+        for (int i = tmpResult.size() - 1; i >= 0; i--) {
+            result.add(tmpResult.get(i));
+        }
     }
 
 

4e7cfd96b469d374ad7fd2a1d8709c896f3958ec
jun.chu <1571970439@qq.com>
2022-05-29 11:30:54 +0800

    refactor(bigdecimaltest): modify

---
+++ src/test/java/com/jun/chu/java/math/BigDecimalTest.java
@@ -0,0 +1,29 @@
+package com.jun.chu.java.math;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+/**
+ * @author chujun
+ * @date 2022/5/29
+ */
+public class BigDecimalTest {
+    @Test
+    public void test() {
+        //阿里巴巴手册这两个是精确的
+        BigDecimal a = new BigDecimal("0.1");
+        BigDecimal b = BigDecimal.valueOf(0.1);
+        double d = 0.1;
+        System.out.println(d);
+        Assert.assertEquals(0, a.compareTo(b));
+        System.out.println(new BigDecimal(0.1));
+    }
+
+    @Test
+    public void test2() {
+        double d = new BigDecimal("1").divide(new BigDecimal("10")).doubleValue();
+        System.out.println(d);
+    }
+}

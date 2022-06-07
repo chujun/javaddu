fa60c3e30b378fdea7076e55cf7cd56bf5c95b1a
jun.chu <1571970439@qq.com>
2022-06-07 10:12:56 +0800

    refactor(testsortmethod): selectionSort

--- src/test/java/com/jun/chu/java/leetcode/TestSortMethod.java
+++ src/test/java/com/jun/chu/java/leetcode/TestSortMethod.java
@@ -12,14 +12,22 @@ import java.util.List;
  */
 public class TestSortMethod {
     @Test
-    public void test() {
+    public void testBubbleSort() {
         List<Integer> list = initList();
         bubbleSort(list);
         System.out.println(list.toString());
     }
 
+    @Test
+    public void testSelectionSort() {
+        List<Integer> list = initList();
+        selectionSort(list);
+        System.out.println(list.toString());
+    }
+
     /**
      * 冒泡排序
+     * 算法核心思想:不断比较相邻元素,如果第一个元素比第二个元素大，就交换他们的位置
      */
     public static <T extends Comparable<T>> void bubbleSort(List<T> list) {
         if (OriginJDKUtil.isEmpty(list)) {
@@ -28,10 +36,32 @@ public class TestSortMethod {
         int len = list.size();
         for (int i = 0; i < len - 1; i++) {
             for (int j = 0; j < len - 1 - i; j++) {
-                if (greatThan(list.get(j), list.get(j + 1))) {        // 相邻元素两两对比
+                // 相邻元素两两对比
+                if (greatThan(list.get(j), list.get(j + 1))) {
+                    // 元素交换
                     swap(list, j, j + 1);
                 }
             }
+        }
+    }
+
+    /**
+     * 选择排序
+     * 算法核心思想：数组分为待排序区和有序区,每次从待排序区取出最大元素放到有序区队尾
+     */
+    public static <T extends Comparable<T>> void selectionSort(List<T> list) {
+        int len = list.size();
+        int minIndex;
+        for (int i = 0; i < len - 1; i++) {
+            minIndex = i;
+            for (int j = i + 1; j < len; j++) {
+                // 寻找最小的数
+                if (greatThan(list.get(minIndex), list.get(j))) {
+                    // 将最小数的索引保存
+                    minIndex = j;
+                }
+            }
+            swap(list, i, minIndex);
         }
     }
 

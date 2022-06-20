60dee272dfae024b6589ad2c810b02a19d78ddda
jun.chu <1571970439@qq.com>
2022-06-20 09:48:46 +0800

    test(redissondemoapplicationtests): first unit pass

--- redissionDemo/src/test/java/com/jun/chu/java/redissionDemo/RedissonDemoApplicationTests.java
+++ redissionDemo/src/test/java/com/jun/chu/java/redissionDemo/RedissonDemoApplicationTests.java
@@ -1,13 +1,22 @@
 package com.jun.chu.java.redissionDemo;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RedissonDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private RedissonClient redissonClient;
+
+    @Test
+    void contextLoads() {
+        redissonClient.getBucket("hello").set("bug");
+        String test = (String) redissonClient.getBucket("hello").get();
+        Assertions.assertEquals(test,"bug");
+    }
 
 }

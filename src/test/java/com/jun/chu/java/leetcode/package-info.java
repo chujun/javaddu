848b6e9b6978bee44f55c5d3ef813649c1397aa8
jun.chu <1571970439@qq.com>
2022-06-25 10:40:28 +0800

    test(ratelimitertest): add unit test

---
+++ src/test/java/com/jun/chu/java/leetcode/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * TODO:cj test package move to source package
+ * @author chujun
+ * @date 2022/6/25
+ */
+package com.jun.chu.java.leetcode;
---
+++ src/test/java/com/jun/chu/java/serviceGovern/rateLimit/guava/RateLimiterTest.java
@@ -0,0 +1,51 @@
+package com.jun.chu.java.serviceGovern.rateLimit.guava;
+
+import com.google.common.util.concurrent.RateLimiter;
+import org.junit.Test;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author chujun
+ * @date 2022/6/25
+ */
+public class RateLimiterTest {
+    @Test
+    public void test() {
+        RateLimiter limiter = RateLimiter.create(1);
+
+        for (int i = 1; i < 10; i = i + 2) {
+            double waitTime = limiter.acquire(i);
+            System.out.println("cutTime=" + System.currentTimeMillis() + " acq:" + i + " waitTime:" + waitTime);
+        }
+    }
+
+    @Test
+    public void test2() throws InterruptedException {
+        // qps设置为5，代表一秒钟只允许处理五个并发请求
+        RateLimiter rateLimiter = RateLimiter.create(5);
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        int nTasks = 10;
+        CountDownLatch countDownLatch = new CountDownLatch(nTasks);
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < nTasks; i++) {
+            final int j = i;
+            executorService.submit(() -> {
+                rateLimiter.acquire(1);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+                System.out.println(Thread.currentThread().getName() + " gets job " + j + " done");
+                countDownLatch.countDown();
+            });
+        }
+        executorService.shutdown();
+        countDownLatch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("10 jobs gets done by 5 threads concurrently in " + (end - start) + " milliseconds");
+
+    }
+}

10cd86828f06f6ae2e7c78217f2fdca88a2afbfa
jun.chu <1571970439@qq.com>
2023-04-26 16:35:04 +0800

    refactor(transmittablethreadlocal): import pom.xml

---
+++ src/main/java/com/jun/chu/java/mulithread/threadlocal/TransmittableThreadLocalInfo.java
@@ -0,0 +1,11 @@
+package com.jun.chu.java.mulithread.threadlocal;
+
+import com.alibaba.ttl.TransmittableThreadLocal;
+
+/**
+ * @author chujun
+ * @date 2023/4/26
+ */
+public class TransmittableThreadLocalInfo {
+    private static TransmittableThreadLocal transmittableThreadLocal = new TransmittableThreadLocal();
+}
